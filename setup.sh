6e1b0aafc21d23eb29abc35af446b27c083dd9be
EsraaNasr92 <esraanasr706@gmail.com>
2020-05-27 15:28:58 +0200

    update DB utl and screenshot folder

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 export AUTH0_DOMAIN='capston.auth0.com'
 export ALGORITHMS=['RS256']
 export API_AUDIENCE='capston'
-export DATABASE_URL='postgres://tyxzuqccfokonr:194f366cda0405fecff4331ed1b1643654c5e47cd178885aaa60db63bc124171@ec2-35-171-31-33.compute-1.amazonaws.com:5432/d3iq930ftck64c'
+export DATABASE_URL='postgres://uwnlsiwjvvceln:22663b3f0b62a3022c646a4a4c903ea183012585ac2b32cc6dbb9ada351e78d2@ec2-54-86-170-8.compute-1.amazonaws.com:5432/d94fpsgvbj97an'
