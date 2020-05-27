0c42bf659569ba94ab78d1bc9df50737e66677c5
EsraaNasr92 <esraanasr706@gmail.com>
2020-05-27 14:05:49 +0200

    database url

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ db = SQLAlchemy()
 
 
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://uwnlsiwjvvceln:22663b3f0b62a3022c646a4a4c903ea183012585ac2b32cc6dbb9ada351e78d2@ec2-54-86-170-8.compute-1.amazonaws.com:5432/d94fpsgvbj97an'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
